185f09f9c15ade93a28303a52201fb2d2a99b0f7
smiiiiin <mimple1997@icloud.com>
2024-07-17 09:05:17 +0900

    Initial Commit

---
+++ code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  code
+//
+//  Created by Uin on 2024/07/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

63d14f171d1074987bc545a1ad8a19619be8d9df
smiiiiin <mimple1997@icloud.com>
2024-08-12 07:58:25 +0900

    상어초

--- code/main.cpp
+++ code/main.cpp
@@ -1,61 +1,113 @@
-// BFS>QUEUE> 16236 아기상어
-/* !! q는 기본 틀 유지하고 pop();기준으로 방송출연(visited=1, 행동)
- 소개해주기 전이나 생방 전에는 필수로 new_face인지 아닌지 보기 */
-// n, map이 주어진다 1-6물고기크기, 아기상어, 빈칸, 자기보다 크지 않으면 지나가고 작아야 먹는다
-// 먹는 순, 최단거리, y위 x위
+//백준 21608
 #include <iostream>
+#include <vector>
 #include <queue>
 using namespace std;
 
-int n; int dy[] = {-1, 0, 0, 1}, dx[] = {0, -1, 1, 0};
-int map[20][20]; // bool  visited[20][20];
-
-struct FISH{ int y,x,time;
-    bool operator<(const FISH f)const{
-        if(f.time==time){
-            if(f.y==y) return f.x<x;
-            return f.y<y;}
-        return f.time<time;}
-    FISH(int a,int b, int c){ y=a; x=b; time=c;}
+struct student {
+    int id;
+    int lover[4];
+    int x, y;
 };
 
-queue<FISH> q;
+int N;
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
 
-int bfs(){
-    int res=0, eat=0,size=2;
-    // q안에 pq이용하려고 q밖에 while(1)쓰고 쓰자마자 pq선언, visited도 바로
-    while (1){
-        priority_queue<FISH> pq; bool visited[20][20]={0,};
-            
-        while (!q.empty()){
-            int cy= q.front().y;int cx= q.front().x;int ct= q.front().time; q.pop();
-            for(int d=0;d<4;d++){
-                int ny = cy+ dy[d]; int nx= cx+dx[d];
-                if(ny>=n||nx>=n||ny<0||nx<0) continue;
-                if(visited[ny][nx]|| map[ny][nx]>size) continue;
-                visited[ny][nx]=1;
-                q.push(FISH(ny,nx,ct+1)); // 필터망
-                if(map[ny][nx]<size && map[ny][nx]!=0){
-                    pq.push(FISH(ny,nx,ct+1));}}}
-        
-        // 이제 q가 비면 pq타임(먹는다) 이 시작된다
-        if(pq.empty()) break;
-        
-        int eatten_cy=pq.top().y; int eatten_cx=pq.top().x;
-        int eatten_ct=pq.top().time; // pq.pop();   //pop을 하면 왜 안될까?
-        
-        eat++; if(eat== size){ size++; eat=0;}
-        map[eatten_cy][eatten_cx]=0;
-        // 다시 q로 넣는다 먹을땐 시간 0소요 ?
-        q.push(FISH(eatten_cy,eatten_cx,0));
-        res+= eatten_ct;}
-    return res;}
+struct info {
+    int x, y, blankCnt=0, friendCnt = 0;
+    bool operator <(const info& i) const {
+        if (friendCnt == i.friendCnt) {
+            if (blankCnt == i.blankCnt) {
+                if (x == i.x) return y > i.y;
+                else return x > i.x;
+            }
+            else return blankCnt < i.blankCnt;
+        }
+        else return friendCnt < i.friendCnt;
+    }
+};
 
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin>>n; for(int y=0;y<n;y++){
-                for(int x=0;x<n;x++){
-                        cin>>map[y][x];
-                        if(map[y][x]==9){
-                            map[y][x]=0; q.push({y,x,0});}}}
-    cout<<bfs()<<"\n"; return 0;}
+
+int satisfied[] = { 0,1,10,100,1000 };
+
+
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
+    int board[20][20] = {0,};
+    cin >> N;
+
+    vector<student> students(N * N);
+    for (int i = 0; i < N * N; i++) {
+        cin >> students[i].id;
+        for (int j = 0; j < 4; j++) {
+            cin >> students[i].lover[j];
+        }
+    }
+
+    // 1. 자리 배치하기
+    for (int s = 0; s < N * N; s++) {
+        priority_queue<info> pQ;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 0) {
+                    int blankCnt = 0, friendCnt = 0;
+                    for (int d = 0; d < 4; d++) {
+                        int nx = i + dx[d];
+                        int ny = j + dy[d];
+
+                        if (nx<0 || nx>N - 1 || ny<0 || ny>N - 1) continue;
+                        if (board[nx][ny] == 0) blankCnt++;
+                        else {
+                            for (int k = 0; k < 4; k++) {
+                                if (board[nx][ny] == students[s].lover[k]) {
+                                    friendCnt++;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    pQ.push({ i,j,blankCnt , friendCnt });
+                }
+            }
+        }
+
+        
+            int x = pQ.top().x;
+            int y = pQ.top().y;
+
+            board[x][y] = students[s].id;
+            students[s].x = x;
+            students[s].y = y;
+        
+    }
+
+    // 2. 만족도 계산
+    int res = 0;
+    for (int s = 0; s < N * N; s++) {
+        int x = students[s].x;
+        int y = students[s].y;
+
+        int friends = 0;
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if (nx<0 || nx>N - 1 || ny<0 || ny>N - 1) continue;
+            for (int k = 0; k < 4; k++) {
+                if (board[nx][ny] == students[s].lover[k]) {
+                    friends++;
+                    break;
+                }
+            }
+        }
+        res += satisfied[friends];
+    }
+
+    cout << res;
+
+    return 0;
+}
+

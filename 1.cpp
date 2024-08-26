0f18c75800e50c5808d708f3001d893fa09e1edc
smiiiiin <mimple1997@icloud.com>
2024-08-27 07:51:26 +0900

    go

--- 1.cpp
+++ 1.cpp
@@ -1,75 +1,47 @@
-// 1605 반복 부분문자열
-#include <iostream>
-#include <vector>
-#include <set>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-#define endl '\n'
-char str[200005];
-const int p = 29;
-const int MOD = 10007;
-int Len;
-int pexp[200005];
-vector<int> hashTable[10007];        //시작 index를 갖는다.
-
-bool real_match(int s1, int s2, int L) {
-    for (int i = 0; i < L; i++) {
-        if (str[s1 + i] != str[s2 + i]) {
-            return false;
+int n, m;
+vector<int> v[10001];
+int depth[10001];
+bool visited[10001];
+int dfs(int i,int cnt) {
+    depth[i] = cnt;
+    int ret = cnt;
+    int child = 0;
+    for (int j : v[i]) {
+        if (depth[j]) {
+            ret = min(ret, depth[j]);
+            continue;
         }
-    } return true;
-}
-
-void pre() {
-    pexp[0] = 1;
-    for (int i = 1; i <= 200000; i++) {
-        pexp[i] = pexp[i - 1] * p % MOD;
-    }
-}
-
-bool ok(int L){
-   
-    for (int i = 0; i < MOD; i++) {
-        hashTable[i].clear();
-    }
-    
-       //길이 L의 중복 부분문자열이 존재하는지를 리턴
-    int hash = 0;
-    for (int i = 0; i < L; i++) {
-        hash *= p;
-        hash += str[i] - 'a';
-        hash %= MOD;
-    }
-    hashTable[hash].push_back(0);
-    for (int i = L; i < Len; i++) {
-        hash -= (str[i - L] - 'a') * pexp[L - 1] % MOD;
-        hash = (hash + MOD) % MOD;
-        hash *= p; hash += str[i] - 'a';
-        hash %= MOD;
-        if (hashTable[hash].size()) {
-            for (int idx : hashTable[hash]) {
-                if (real_match(idx, i - L + 1, L)) return true;
-            }
+        child++;
+        int prev=dfs(j, cnt + 1);
+        if (depth[i] != 1 && prev >= cnt) {
+            visited[i] = 1;
         }
-        hashTable[hash].push_back(i-L+1);
+        ret = min(ret, prev);
     }
-    return false;
+    if (depth[i] == 1 && child >= 2)visited[i] = true;
+    return ret;
 }
-
-void solve() {
-    scanf("%s", str);
-    int l = 0, r = Len;
-    while (l < r) {
-        int m = (l + r + 1) / 2;
-        if (ok(m)) l = m;
-        else r = m - 1;
+int main(void) {
+    ios::sync_with_stdio(false);    cin.tie(NULL);    cout.tie(NULL);
+    cin >> n >> m;
+    for (int i = 0, a,b; i < m; i++) {
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
     }
-    cout << l << endl;
-}
+    for (int i = 1; i <= n; i++) {
+        if (!depth[i]) {
+            dfs(i, 1);
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++)if (visited[i + 1])ans++;
+    cout << ans << '\n';
+    for (int i = 0; i < n; i++)if (visited[i + 1])cout << i + 1 << " ";
     
-
-int main() {
-    pre();
-    cin >> Len;
-    solve();
+    return 0;
 }
-

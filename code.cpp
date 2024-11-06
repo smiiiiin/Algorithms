f437d4fa14663813a6aad4bb98ca4a5f324ab757
smiiiiin <mimple1997@icloud.com>
2024-11-06 10:53:13 +0900

    dd

---
+++ code.cpp
@@ -0,0 +1,8 @@
+//
+//  code.cpp
+//  코드판
+//
+//  Created by Uin on 2024/10/07.
+//
+
+#include "code.hpp"
---
+++ code.hpp
@@ -0,0 +1,13 @@
+//
+//  code.hpp
+//  코드판
+//
+//  Created by Uin on 2024/10/07.
+//
+
+#ifndef code_hpp
+#define code_hpp
+
+#include <stdio.h>
+
+#endif /* code_hpp */
